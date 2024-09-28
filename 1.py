1a180a14ec17810ebdef1eb6fa9ea813d004965b
Matvey133 <mateyhcerevatov9@gmail.com>
2024-09-28 19:31:19 +0300

    Add File

---
+++ 1.py
@@ -0,0 +1,15 @@
+#1st program
+print(9**0.5*5)
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+#4th program
+m="123.456"
+n=float("123.456")
+n=n*10
+a=int(n)
+a=a%10
+print(a)
